e4eee7b75376b1c126297e0b406c56e35a5c93f8
Caitlin.Hall2 <Caitlin.Hall2@target.com>
2015-02-04 11:39:46 -0600

    Getting ready to add instagram to site

--- web.js
+++ web.js
@@ -2,10 +2,34 @@ require('newrelic');
 var express = require('express');
 var app = express();
 var path = require('path');
+//var api = require('instagram-node').instagram();
 
 app.use(express.logger());
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+/*api.use({
+  client_id: process.env.INSTAGRAM_CLIENT_ID,
+  client_secret: process.env.INSTAGRAM_CLIENT_SECRET
+});
+
+var userId = process.env.INSTAGRAM_USER_ID;
+
+// endpoint
+app.get('/instagram', function (req, res, next) {
+  var count = req.query.count || 1;
+
+  // example: call user_media_recent to get lastest user posts
+  api.user_media_recent(userId, { count: count }, function (err, medias, pagination, remaining, limit) {
+    if (err) {
+      next(err);
+    } else {
+    	//var getImages = medias.map(medias.data);
+
+      res.send(medias);
+    }
+  });
+});*/
 
 var port = process.env.PORT || 5000;
 
