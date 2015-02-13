45d860341bae703f143452a9611e39f9a888d13a
Caitlin Hall <caitlin.hall2@target.com>
2015-02-13 12:05:53 -0600

    adding instagram!!!! :)

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,13 @@
-
+$(document).ready(function(){
+	$.ajax({
+		url: "/instagram",
+	}).done(function(data) {
+		var parsed = JSON.parse(data);
+		var picContainer = $("#picContainer");
+		if(picContainer){
+			for(var i = 0; i < parsed.length; i++){
+		        picContainer.append("<img id='grid' src='" + parsed[i] + "'alt='pic' />");
+		    }
+		}
+	});
+});
