61039532565481b6e92026a7f834ace779af7246
Caitlin Hall <caitlin.hall2@target.com>
2015-12-11 14:24:28 -0600

    removed insta and updated so info

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,4 @@
 $(document).ready(function(){
-	$.ajax({
-		url: "/instagram",
-	}).done(function(data) {
-		var parsed = JSON.parse(data);
-		var picContainer = $("#picContainer");
-		if(picContainer){
-			for(var i = 0; i < parsed.length; i++){
-		        picContainer.append("<img id='grid' src='" + parsed[i] + "'alt='pic' />");
-		    }
-		}
-	});
 	
 	$('a[href*=#]:not([href=#])').click(function() {
 		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
